b9a08e41d84f68ea553f7e459345071de1a4e461
ferbass <ferbass@gmail.com>
2015-07-07 22:28:43 -0300

    change boot2docker to docker-machine

--- zsh/docker.zsh
+++ zsh/docker.zsh
@@ -1,4 +1,7 @@
-$(boot2docker shellinit)
+docker-machine start
+eval "$(docker-machine env dev)"
+
 export DOCKER_TLS_VERIFY=1
-export DOCKER_HOST=tcp://192.168.59.103:2376
-export DOCKER_CERT_PATH="$HOME/.boot2docker/certs/boot2docker-vm"
+export DOCKER_CERT_PATH="/Users/ferbass/.docker/machine/machines/dev"
+export DOCKER_HOST=tcp://192.168.99.100:2376
+
