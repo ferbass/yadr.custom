7406cdd522fea2b99ad31bee7148e62d5186137e
ferbass <ferbass@gmail.com>
2015-07-08 09:13:16 -0300

    change fix path to /Users/ferbass

--- zsh/docker.zsh
+++ zsh/docker.zsh
@@ -1,7 +1,7 @@
-docker-machine start
+docker-machine start dev
 eval "$(docker-machine env dev)"
 
 export DOCKER_TLS_VERIFY=1
-export DOCKER_CERT_PATH="/Users/ferbass/.docker/machine/machines/dev"
+export DOCKER_CERT_PATH="$HOME/.docker/machine/machines/dev"
 export DOCKER_HOST=tcp://192.168.99.100:2376
 
