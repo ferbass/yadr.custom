214b83ecd5d5d2b816e31d46534ec0a48f4ba28b
ferbass <ferbass@gmail.com>
2021-05-25 07:49:25 +0900

    add pyenv init --path

--- zsh/pyenv.zsh
+++ zsh/pyenv.zsh
@@ -1,5 +1,6 @@
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
 
 if command -v pyenv 1>/dev/null 2>&1; 
 then
@@ -9,3 +10,5 @@ fi
 export PATH="/usr/local/opt/python/libexec/bin:$PATH"
 
 alias pip=/usr/local/bin/pip3
+
+
